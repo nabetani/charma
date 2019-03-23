1050189535d73195a6c5ec77bb21dfd04666881a
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-23 20:45:01 +0900

    サンプルの形式を変更する

--- charma/examples/simple_bar.rb
+++ charma/examples/simple_bar.rb
@@ -4,15 +4,20 @@ require 'charma'
 
 Charma::Document.new{ |doc|
   [
-    [ [3,1,4,1,5], "Lorem ipsum" ],
-    [ [9,2,6,5,4,5,8,9,7,9], nil ],
-    [ [76, -28, -67, 155, 77, 160, 61, 48, -63, 26], "Excepteur sint occaecat cupidatat non proident"],
-  ].each do |y, title|
+    {
+      y_values: [3,1,4,1,5],
+      title: "Lorem ipsum",
+    },
+    {
+      y_values: [9,2,6,5,4,5,8,9,7,9],
+    },
+    {
+      y_values: [76, -28, -67, 155, 77, 160, 61, 48, -63, 26],
+      title: "Excepteur sint occaecat cupidatat non proident",
+    }
+  ].each do |opts|
     doc.new_page do |page|
-      page.add_barchart(
-        y_values: y,
-        title: title
-      )
+      page.add_barchart( opts )
     end
   end
 }.render( "simple_bar.pdf" )
