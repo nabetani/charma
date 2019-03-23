22777f95155b3ea31fbecd937b6181376a2c9862
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-23 22:13:11 +0900

    サンプルデータを追加する

--- charma/examples/simple_bar.rb
+++ charma/examples/simple_bar.rb
@@ -16,7 +16,11 @@ Charma::Document.new{ |doc|
       y_values: [76, -28, -67, 155, 77, 160, 61, 48, -63, 26],
       title: "Excepteur sint occaecat cupidatat non proident",
       x_ticks:%w(foo bar baz qux quux corge grault garply waldo fred),
-    }
+    },
+    {
+      y_values: [7e+06, 2.8e+07, 1.2e+07, 1.5e+07, 2.7e+07, 2.5e+07, 1.3e+07],
+      x_ticks:%w(Alcyone Atlas Electra Maia Merope Taygeta Pleione),
+    },
   ].each do |opts|
     doc.new_page do |page|
       page.add_barchart( opts )
