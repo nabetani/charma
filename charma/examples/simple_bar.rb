4357c44f046711632383e21ceeb5afe2a9e0c1f9
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-23 20:40:36 +0900

    負の値とタイトルを付加

--- charma/examples/simple_bar.rb
+++ charma/examples/simple_bar.rb
@@ -3,14 +3,16 @@
 require 'charma'
 
 Charma::Document.new{ |doc|
-  doc.new_page do |page|
-    page.add_barchart(
-      y_values:[3,1,4,1,5]
-    )
-  end
-  doc.new_page do |page|
-    page.add_barchart(
-      y_values:[9,2,6,5,4,5,8,9,7,9]
-    )
+  [
+    [ [3,1,4,1,5], "Lorem ipsum" ],
+    [ [9,2,6,5,4,5,8,9,7,9], nil ],
+    [ [76, -28, -67, 155, 77, 160, 61, 48, -63, 26], "Excepteur sint occaecat cupidatat non proident"],
+  ].each do |y, title|
+    doc.new_page do |page|
+      page.add_barchart(
+        y_values: y,
+        title: title
+      )
+    end
   end
 }.render( "simple_bar.pdf" )
