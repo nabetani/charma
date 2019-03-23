2250b8b9aab4b7bbdea34c800108734df2cce375
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-23 18:20:35 +0900

    ページ内に矩形を出力する

--- charma/examples/simple_bar.rb
+++ charma/examples/simple_bar.rb
@@ -5,7 +5,12 @@ require 'charma'
 Charma::Document.new{ |doc|
   doc.new_page do |page|
     page.add_barchart(
-      y_values:[3,1,4,1,5],
+      y_values:[3,1,4,1,5]
+    )
+  end
+  doc.new_page do |page|
+    page.add_barchart(
+      y_values:[9,2,6,5,4,5,8,9,7,9]
     )
   end
 }.render( "simple_bar.pdf" )
--- charma/lib/charma.rb
+++ charma/lib/charma.rb
@@ -4,6 +4,14 @@ require "prawn"
 module Charma
   class Error < StandardError; end
 
+  Rect = Struct.new( :x, :y, :w, :h ) do
+    def stroke(pdf)
+      pdf.stroke{
+        pdf.rectangle( [x, y], w, h )
+      }
+    end
+  end
+
   class Chart
   end
 
@@ -11,12 +19,34 @@ module Charma
     def initialize(opts)
       @opts = opts
     end
+
+    def render( pdf, rect )
+      rect.stroke(pdf)
+    end
   end
 
   class Page
     def initialize( doc )
       @doc = doc
       @graphs = []
+    end
+
+    def create_opts
+      {
+        page_size: "A4",
+        page_layout: :landscape,
+      }
+    end
+
+    def area(mb, _)
+      Rect.new(mb.left, mb.top, mb.width, mb.height)
+    end
+
+    def render(pdf)
+      pdf.stroke_axis
+      @graphs.each.with_index do |g,ix|
+        g.render( pdf, area(pdf.margin_box, ix) )
+      end
     end
 
     def add_barchart(opts)
@@ -38,8 +68,13 @@ module Charma
     end
 
     def render( filename )
-      Prawn::Document.generate(filename) do |pdf|
-        pdf.text( "hoge" )
+      raise "no page added" if @pages.empty?
+      opts = @pages.first.create_opts
+      Prawn::Document.generate(filename, opts) do |pdf|
+        @pages.each.with_index do |page,ix|
+          pdf.start_new_page(page.create_opts) if ix != 0
+          page.render(pdf)
+        end
       end
     end
   end
