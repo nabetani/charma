a0ced9e7640aa76734d9f5683340b8144e314160
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-24 17:56:52 +0900

    不要な Spec を撤去する

--- charma/spec/charma_spec.rb
+++ charma/spec/charma_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Charma do
   it "has a version number" do
     expect(Charma::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
