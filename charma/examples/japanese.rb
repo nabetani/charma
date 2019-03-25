a3d12c88fb30b596bde6bc4aa2295a11e893e507
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-03-25 22:30:50 +0900

    日本語フォントを指定するサンプルを入れる

---
+++ charma/examples/japanese.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'charma'
+
+fontpath = "~/Library/Fonts/ipaexg.ttf"
+
+def macos?
+  case RbConfig::CONFIG['host_os']
+  when /darwin/, /mac\s*os/i
+    true
+  else
+    false
+  end
+end
+
+unless macos?
+  $stderr.puts( "This sample runs only on macOS" )
+  exit
+end
+
+unless File.exist?( File.expand_path(fontpath) )
+  $stderr.puts( "このサンプルを実行するには、IPAexゴシック Regular をインストールする必要があります" )
+  exit
+end
+
+Charma::Document.new( font:"~/Library/Fonts/ipaexg.ttf" ) do |doc|
+  doc.new_page do |page|
+    page.add_barchart( 
+      series:[{y:[3,1,4,1,5]}],
+      x_ticks:%w[ ほげ ふが ぴよ 魑魅魍魎 𩸽と𩹉 ],
+    )
+  end
+  doc.render( File.basename(__FILE__, ".*")+".pdf" )
+end
