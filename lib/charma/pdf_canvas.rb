fb42c01777b2b00f384e256b8b14e032261d9bf2
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-04-27 17:45:53 +0900

    shrink_to_fit を使わず、自分で計算する

--- lib/charma/pdf_canvas.rb
+++ lib/charma/pdf_canvas.rb
@@ -26,21 +26,25 @@ module Charma
 
     def text( t, rect, opts = {} )
       pr = pdf_rect(rect)
+      w = @pdf.width_of(t, size:1)
+      h = @pdf.height_of(t, size:1)
+      font_size = [pr.w.to_f/w ,pr.h.to_f/h].min * 0.95
+
+
+
       @pdf.bounding_box([pr.x, pr.bottom], width:pr.w, height:pr.h ) do
         @pdf.transparent(1) { @pdf.stroke_bounds }
-        @pdf.font_size 16
       end
-      @pdf.text_box( t,
+
+      @pdf.text_box(
+        t,
         at:[pr.x, pr.bottom],
         width:pr.w,
         height:pr.h,
         align: :center,
         valign: :center,
-        size: rect.h*2,
-        overflow: :shrink_to_fit )
-
-
-
+        size:font_size
+      )
     end
   end
 end
