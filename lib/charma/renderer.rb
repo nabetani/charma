37dbeb2c3fbb22345449078ad201b8ca21970054
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-04-27 20:48:01 +0900

    一つのページに複数のチャートを描画する

--- lib/charma/renderer.rb
+++ lib/charma/renderer.rb
@@ -16,13 +16,25 @@ module Charma
       end
     end
 
+    def split_page( total, count )
+      xcount = (1..count ).min_by{ |w|
+        h = ( count.to_r / w.to_r ).ceil
+        cw = total.w.to_f / w
+        ch = total.h.to_f / h
+        Math.log(cw/ch).abs
+      }
+      ycount = ( count.to_r / xcount.to_r ).ceil
+      total.vsplit( *Array.new(ycount, 1) ).map{ |rc|
+        rc.hsplit( *Array.new(xcount, 1) )
+      }.flatten
+    end
+
     def render_page( canvas, page )
       canvas.new_page(page)
-      page_rect = canvas.page_rect
-      p page_rect
-      page.charts.each do |chart|
+      rects = split_page( canvas.page_rect, page.charts.size )
+      page.charts.zip(rects).each do |chart, rect|
         t = chart_renderer(chart.chart_type)
-        t.new( chart, canvas, page_rect ).render
+        t.new( chart, canvas, rect ).render
       end
     end
   end
