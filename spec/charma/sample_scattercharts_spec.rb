fbcf92b131134560c71b1b50bf9f9f14b5a0fd23
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-05-19 11:57:19 +0900

    対数グラフのサンプルを追加する

--- spec/charma/sample_scattercharts_spec.rb
+++ spec/charma/sample_scattercharts_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe :ScatterChartSamples do
     expect( File.exist?( path ) ).to be true
   end
 
-  it "dot count" do |example|
+  it "various dot count" do |example|
     vals = lambda do |c,n|
       rot = c**0.3
       Array.new(c) do |ix|
@@ -45,6 +45,33 @@ RSpec.describe :ScatterChartSamples do
         series: Array.new(ix+1){ |serinum|
           {name:"series ##{serinum}", xy:vals[count,serinum]}
         }
+      )
+    end
+    Charma::Document.new do |doc|
+      doc.add_page do |page|
+        charts.each do |chart|
+          page.add_chart( chart )
+        end
+      end
+      doc.render( path )
+    end
+    expect( File.exist?( path ) ).to be true
+  end
+
+  it "log10 scale" do |example|
+    path = makepath(example, ".pdf" )
+    charts = Array.new(4) do |ix|
+      x = ix[0]==0 ? :linear : :log10
+      xf = ix[0]==0 ? ->(v){ v } : ->(v){ 10**v }
+      y = ix[1]==0 ? :linear : :log10
+      yf = ix[1]==0 ? ->(v){ v } : ->(v){ 10**v }
+      Charma::ScatterChart.new(
+        x_scale: x,
+        y_scale: y,
+        title: "x:#{x}, y:#{y}",
+        series: [
+          {xy:(-1..4).map{ |e| [xf[e], yf[e]] }}
+        ]
       )
     end
     Charma::Document.new do |doc|
