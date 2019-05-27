546e251ffd223fa821ecabc3ba517448bf53b576
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-05-27 23:41:00 +0900

    ViolinChartRenderer の new と calc_yrange の一部を実装

---
+++ lib/charma/violinchart_renderer.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module Charma
+  # バイオリンチャートを描画する
+  class ViolinChartRenderer < ChartRenderer
+
+    # 描画オブジェクトを生成する
+    def initialize( chart, canvas, area )
+      super
+    end
+
+    # x_ticks 領域があるかどうか
+    def x_ticks_area?
+      !! @chart[:x_ticks]
+    end
+
+    # x_marks 領域があるかどうか
+    def x_marks_area?
+      false
+    end
+
+    def expand_range( axis, range, width )
+      min, max = range.minmax
+      xmin = min-width
+      xmax = max+width
+      ignore_zero_pos = scale_type(axis)==:log10
+      if ignore_zero_pos
+        [xmin, xmax]
+      elsif 0<xmin
+        [xmin, xmax]
+      elsif 0<=min
+        [0, xmax]
+      elsif 0<max
+        [xmin, xmax]
+      elsif 0<=xmax
+        [xmin, 0]
+      else
+        [xmin, xmax]
+      end
+    end
+
+    # y と y2 の範囲を計算する
+    # 返戻値の第一要素が yの範囲。第二要素が y2の範囲。y2 がない場合はnil
+    # ※ 対数目盛に対応する
+    # ※ y と y2 の y==0 の描画座標が等しくなるようにはしない
+    def calc_yranges
+      yrange, y2range = %i(y y2).map{ |sym|
+        v = @chart[:series].flat_map{ |s|
+          s[sym]&.map{ |vals| 
+            vals.flatten.map{ |val|
+              scale_value( sym, val )
+            }
+          }
+        }.flatten.compact
+        if v.empty?
+          nil
+        else
+          candidate = v.minmax
+          if candidate[0]==candidate[1]
+            expand_range( sym, candidate, 1 ) # ゼロ除算対策
+          else
+            diff = candidate[1]-candidate[0]
+            expand_range( sym, candidate, diff*0.099 )
+          end
+        end
+      }
+    end
+  end
+end
---
+++ spec/charma/violinchart_renderer_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Charma::ViolinChartRenderer do
+  def series_yrange( y, y2=nil )
+    seri = lambda do |name,vals|
+      mid = vals.sum / vals.size.to_f
+      { name=>[[ mid, *vals, mid ]] }
+    end
+    r=[]
+    r.push seri[:y, y]
+    r.push seri[:y2, y2] if y2
+    r
+  end
+
+  def rect01
+    Charma::Rect.new( 0, 0, 1, 1)
+  end
+
+  describe ".new" do
+    it "creates ViolinChartRenderer" do
+      chart = Charma::ViolinChart.new(series:[{y:[[1]]}])
+      expect{
+        Charma::ViolinChartRenderer.new( chart, nil, Charma::Rect.new( 0, 0, 1, 1) )
+      }.not_to raise_error
+    end
+  end
+
+  describe "#calc_yranges" do
+    describe "without y2" do
+      EXAMPLES = [
+        [[0,1.099], [0,1]], # 全て0以上なら範囲も0以上になる
+        [[-1.099, 0], [0,-1]], # 全て0以下なら範囲も0以下になる
+        [[-5.99, 5.99], [-5,5]], # 範囲が正負に渡っているなら普通に拡張される
+        [[0,1.0991], [0.0001,1.0001]], # 全て0以上なら範囲も0以上になる
+        [[-1.0991, 0], [-0.0001,-1.0001]], # 全て0以下なら範囲も0以下になる
+        [[100-0.099, 101+0.099], [100, 101]], # 範囲が余裕を持って正なら、普通に拡張される
+        [[-101-0.099, -100+0.099], [-100, -101]], # 範囲が余裕を持って負なら、普通に拡張される
+      ]
+
+      LOG_EXAMPLES = [
+        [[-0.099,1.099], [0,1]], # 正負に関係なく普通に拡張される
+        [[10-0.099,10+1.099], [10,11]], # 普通に拡張される
+        [[-0.099*10,1.099*10], [0,10]], # 正負に関係なく普通に拡張される
+      ]
+
+      EXAMPLES.each do |expected, input|
+        it "returns #{expected.inspect} if y-values are in #{input.inspect}" do
+          chart = Charma::ViolinChart.new(series:series_yrange(input))
+          r = Charma::ViolinChartRenderer.new( chart, nil, rect01 )
+          yrange, y2range = r.calc_yranges
+          expect( yrange ).to almost_eq_ary( expected, 1e-7 )
+          expect( y2range ).to be_nil
+        end
+      end
+    end
+  end
+
+end
+
