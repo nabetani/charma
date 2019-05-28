c6b5d1344efe8ecd27176bccfe407dc5effe454c
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-05-28 21:10:19 +0900

    入れ子関係がおかしかったので修正

--- spec/charma/violinchart_renderer_spec.rb
+++ spec/charma/violinchart_renderer_spec.rb
@@ -28,33 +28,34 @@ RSpec.describe Charma::ViolinChartRenderer do
   end
 
   describe "#calc_yranges" do
+    EXAMPLES = [
+      [[0,1.099], [0,1]], # 全て0以上なら範囲も0以上になる
+      [[-1.099, 0], [0,-1]], # 全て0以下なら範囲も0以下になる
+      [[-5.99, 5.99], [-5,5]], # 範囲が正負に渡っているなら普通に拡張される
+      [[0,1.0991], [0.0001,1.0001]], # 全て0以上なら範囲も0以上になる
+      [[-1.0991, 0], [-0.0001,-1.0001]], # 全て0以下なら範囲も0以下になる
+      [[100-0.099, 101+0.099], [100, 101]], # 範囲が余裕を持って正なら、普通に拡張される
+      [[-101-0.099, -100+0.099], [-100, -101]], # 範囲が余裕を持って負なら、普通に拡張される
+    ]
+
+    LOG_EXAMPLES = [
+      [[-0.099,1.099], [0,1]], # 正負に関係なく普通に拡張される
+      [[10-0.099,10+1.099], [10,11]], # 普通に拡張される
+      [[-0.099*10,1.099*10], [0,10]], # 正負に関係なく普通に拡張される
+    ]
     describe "without y2" do
-      EXAMPLES = [
-        [[0,1.099], [0,1]], # 全て0以上なら範囲も0以上になる
-        [[-1.099, 0], [0,-1]], # 全て0以下なら範囲も0以下になる
-        [[-5.99, 5.99], [-5,5]], # 範囲が正負に渡っているなら普通に拡張される
-        [[0,1.0991], [0.0001,1.0001]], # 全て0以上なら範囲も0以上になる
-        [[-1.0991, 0], [-0.0001,-1.0001]], # 全て0以下なら範囲も0以下になる
-        [[100-0.099, 101+0.099], [100, 101]], # 範囲が余裕を持って正なら、普通に拡張される
-        [[-101-0.099, -100+0.099], [-100, -101]], # 範囲が余裕を持って負なら、普通に拡張される
-      ]
-
-      LOG_EXAMPLES = [
-        [[-0.099,1.099], [0,1]], # 正負に関係なく普通に拡張される
-        [[10-0.099,10+1.099], [10,11]], # 普通に拡張される
-        [[-0.099*10,1.099*10], [0,10]], # 正負に関係なく普通に拡張される
-      ]
-
-      EXAMPLES.each do |expected, input|
-        it "returns #{expected.inspect} if y-values are in #{input.inspect}" do
-          chart = Charma::ViolinChart.new(series:series_yrange(input))
-          r = Charma::ViolinChartRenderer.new( chart, nil, rect01 )
-          yrange, y2range = r.calc_yranges
-          expect( yrange ).to almost_eq_ary( expected, 1e-7 )
-          expect( y2range ).to be_nil
+      describe "linear scale" do
+        EXAMPLES.each do |expected, input|
+          it "returns #{expected.inspect} if y-values are in #{input.inspect}" do
+            chart = Charma::ViolinChart.new(series:series_yrange(input))
+            r = Charma::ViolinChartRenderer.new( chart, nil, rect01 )
+            yrange, y2range = r.calc_yranges
+            expect( yrange ).to almost_eq_ary( expected, 1e-7 )
+            expect( y2range ).to be_nil
+          end
         end
       end
-      describe "without y2, log scale" do
+      describe "log scale" do
         LOG_EXAMPLES.each do |ex, y|
           real_ex = ex.map{ |e| 10**e }
           real_y = y.map{ |e| 10**e }
