761dffbad291371e0e030ce28ddf8f97ea2b9eb9
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-06-01 23:56:03 +0900

    bins がとても多いサンプルを追加する

--- spec/charma/sample_violincharts_spec.rb
+++ spec/charma/sample_violincharts_spec.rb
@@ -12,18 +12,31 @@ RSpec.describe :ViolinChartSamples do
 
   it "simple violin chart" do |example|
     path = makepath(example, ".pdf" )
-    opts=[]
-    opts.push( {
+    opt ={
       bins:10,
       series:[
         {y:[[1,2,2],[3,2,2],[3,3,2]]},
         {y:[[3,3,5],[4,4,4],[3,4,10]]},
       ]
-    } )
+    }
+    Charma::Document.new do |doc|
+      doc.add_page do |page|
+        page.add_chart( Charma::ViolinChart.new(opt) )
+      end
+      doc.render( path )
+    end
+    expect( File.exist?( path ) ).to be true
+  end
+
+  it "various violin chart" do |example|
+    path = makepath(example, ".pdf" )
+    opts = []
     opts.push(
       bins:300,
+      x_ticks:%w(foo bar baz),
       series:Array.new(2){ |s|
         {
+
           name:"s=#{s}",
           y:Array.new(3){ |x|
             Array.new(1000){ |i|
@@ -48,7 +61,7 @@ RSpec.describe :ViolinChartSamples do
     ]
     opts.push(
       x_ticks:%w(foo bar),
-        bins:100,
+      bins:100,
       series:procs.map{ |proc|
         {
           y:Array.new(2){ |x|
@@ -74,6 +87,21 @@ RSpec.describe :ViolinChartSamples do
           ]
         }
       }
+    )
+    opts.push(
+      title:"title",
+      bins:2000,
+      x_ticks:%w( foo bar baz qux quux ),
+      series:[
+        {
+          y:Array.new(5){ |x|
+            e = (2**x)
+            Array.new(1000){ |i|
+              Array.new(11){rng.rand + i % e}.sum / e
+            }
+          }
+        }
+      ]
     )
     Charma::Document.new do |doc|
       doc.add_page do |page|
