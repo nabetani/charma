10295de378ff498b2aea694fa15eac816a9f9773
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-06-01 15:28:36 +0900

    バイオリンチャートの仕様変更に伴う spec変更

--- spec/charma/violin_chart_spec.rb
+++ spec/charma/violin_chart_spec.rb
@@ -24,13 +24,15 @@ RSpec.describe Charma::ViolinChart do
         series:[S0,S1],
         x_ticks:%w(foo bar baz),
         x_title:"X TITLE",
-        y_title:"Y TITLE"
+        y_title:"Y TITLE",
+        bins:30
       )
       expect(c[:title]).to eq( "TITLE" )
       expect(c[:series]).to eq( [S0,S1] )
       expect(c[:x_ticks]).to eq( %w(foo bar baz) )
       expect(c[:x_title]).to eq( "X TITLE")
       expect(c[:y_title]).to eq( "Y TITLE")
+      expect(c[:bins]).to eq(30)
     end
 
     it "raises if no series" do
@@ -82,42 +84,6 @@ RSpec.describe Charma::ViolinChart do
         Charma::ViolinChart.new({series:[{y:[[[1]]]}]})
       }.to raise_error( Charma::Errors::InvalidOption, pat )
     end
-    it "raises if series.y2 is NOT Array of Numeric" do
-      expect{
-        Charma::ViolinChart.new({series:[{y:[[1]]}, {y2:[[1]]}]})
-      }.not_to raise_error
-      pat = /(Series\.y2\s)|(\sy2\s)/
-      # array of numeric
-      expect{
-        Charma::ViolinChart.new({series:[{y:[[1]]}, {y2:[1]}]})
-      }.to raise_error( Charma::Errors::InvalidOption, pat )
-
-      # array of array of ( numeric and string )
-      expect{
-        Charma::ViolinChart.new({series:[{y:[[1]]}, {y2:[[1,""]]}]})
-      }.to raise_error( Charma::Errors::InvalidOption, pat )
-
-      # array of ( array of numeric and array of string )
-      expect{
-        Charma::ViolinChart.new({series:[{y:[[1]]}, {y2:[[1], [""]]}]})
-      }.to raise_error( Charma::Errors::InvalidOption, pat )
-
-      # array of array of array of numeric
-      expect{
-        Charma::ViolinChart.new({series:[{y:[[1]]}, {y2:[[[1]]]}]})
-      }.to raise_error( Charma::Errors::InvalidOption, pat )
-    end
-    it "raises if a series has both y and y2" do
-      expect{
-        Charma::ViolinChart.new({series:[{y:[[2]], y2:[[1]]}]})
-      }.to raise_error( Charma::Errors::InvalidOption, /both y and y2/ )
-    end
-
-    it "raises if no series has y" do
-      expect{
-        Charma::ViolinChart.new({series:[{y2:[[1]]}, {y2:[[1]]}]})
-      }.to raise_error( Charma::Errors::InvalidOption, /At least one series has y/ )
-    end
 
     it "raises if there is unexpected key" do
       expect{
@@ -129,9 +95,14 @@ RSpec.describe Charma::ViolinChart do
       expect{
         Charma::ViolinChart.new(series:[{y:[[1]], unexpected:1}])
       }.to raise_error( Charma::Errors::InvalidOption, /is not valid key/ )
+
+      # violin chart does not accept y2
+      expect{
+        Charma::ViolinChart.new({series:[{y:[[1]]}, {y2:[[1]]}]})
+      }.to raise_error( Charma::Errors::InvalidOption, /is not valid key/ )
     end
 
-    it "will raise unless x_ticks are Array or nil" do
+    it "raises unless x_ticks are Array or nil" do
       expect{
         Charma::ViolinChart.new(x_ticks:["a"], series:[{y:[[1]]}])
       }.not_to raise_error
@@ -144,28 +115,59 @@ RSpec.describe Charma::ViolinChart do
         Charma::ViolinChart.new(x_ticks:[1], series:[{y:[[1]]}])
       }.not_to raise_error
     end
+
+    describe "bins property" do
+      [
+        { bins:10 },
+        { bins:12.0 }, # accept Float if the value is integer
+        { bins:15r }, # accept Rational if the value is integer
+      ].each do |bins:|
+        it "does not raise if bins is #{bins}" do
+          expect{
+            Charma::ViolinChart.new(bins:bins, series:[{y:[[1]]}])
+          }.not_to raise_error
+        end
+      end
+
+      [
+        { bins:0, pat:/should be positive integer/ },
+        { bins:12.5, pat:/should be positive integer/ },
+        { bins:15.6r, pat:/should be positive integer/ },
+        { bins:-12.5, pat:/should be positive integer/ },
+        { bins:-15.6r, pat:/should be positive integer/ },
+      ].each do |bins:, pat:|
+        it "raises if bins is #{bins}" do
+          expect{
+            Charma::ViolinChart.new(bins:bins, series:[{y:[[1]]}])
+          }.to raise_error( Charma::Errors::InvalidOption, pat )
+        end
+      end
+    end
   end
+
   describe "#chart_type" do
     it "returns :violin_chart" do
       c = Charma::ViolinChart.new(series:[{y:[[1]]}])
       expect(c.chart_type).to eq(:violin_chart)
     end
   end
+
   describe "#y2?" do
-    it "returns false if no series has y2" do
+    it "returns false" do
       c = Charma::ViolinChart.new(series:[{y:[[1]]}])
       expect(c.y2?).to be false
     end
-    it "returns true if some series has y2" do
-      (1..10).each do |pattern|
-        pat = pattern.to_s(2).chars
-        next if pat.uniq.size==1
-        s = pat.map{ |c|
-          { ( c=="0" ? :y : :y2 )=>[[1]] }
-        }
-        c = Charma::ViolinChart.new(series:s)
-        expect(c.y2?).to be true
-      end
+  end
+
+  describe "#bins" do
+    it "returns 100 unless bins is not specified" do
+      c = Charma::ViolinChart.new(series:[{y:[[1]]}])
+      expect(c.bins).to be 100
+    end
+
+    it "returns specified value" do
+      c = Charma::ViolinChart.new(bins:1234, series:[{y:[[1]]}])
+      expect(c.bins).to be 1234
     end
   end
 end
